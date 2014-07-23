bf46915ce2bc62358da6b8a1c136526e17166563
Gabe Radovsky <gabriel.radovsky@gmail.com>
2014-07-23 15:00:47 -0700

    add UsersController

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,18 @@
+class UsersController < ApplicationController
+  def new; end
+  
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      sign_in!(user)
+      redirect_to root_url
+    else
+      flash.now[:errors] = @user.errors.full_messages
+      render :new
+    end
+    
+    private
+    def user_params
+      params.require(:user).permit(:username, :password, :location, :bio)
+    end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140723044212) do
+ActiveRecord::Schema.define(version: 20140723213049) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -26,5 +26,18 @@ ActiveRecord::Schema.define(version: 20140723044212) do
     t.datetime "created_at"
     t.datetime "updated_at"
   end
+
+  create_table "users", force: true do |t|
+    t.string   "username",        null: false
+    t.string   "password_digest", null: false
+    t.string   "session_token",   null: false
+    t.string   "location"
+    t.string   "bio"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "users", ["session_token"], name: "index_users_on_session_token", using: :btree
+  add_index "users", ["username"], name: "index_users_on_username", using: :btree
 
 end
