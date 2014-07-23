737aed0e0b3b707badf12d5bbd4538395a2df05a
Gabe Radovsky <gabriel.radovsky@gmail.com>
2014-07-22 23:51:48 -0700

    add app_router

--- app/assets/javascripts/routers/app_router.js
+++ app/assets/javascripts/routers/app_router.js
@@ -1,3 +1,23 @@
 Kelp.Routers.AppRouter = Backbone.Router.extend({
-
+	routes: {
+		'': 'businessesIndex'
+	},
+	
+	initialize: function($rootEl) {
+		this.$roolEl = $rootEl;
+	},
+	
+	_swapView: function(newView) {
+		if (this._currentView) this._currentView.remove();
+		$('#content').html(newView.render().$el);
+		this._currentView = newView;
+	},
+	
+	businessesIndex: function() {
+		var businessesIndexView = new Kelp.Views.BusinessesIndex({
+			collection: Kelp.businesses
+		});
+		Kelp.businesses.fetch();
+		this._swapView(businessesIndexView);
+	}
 });
