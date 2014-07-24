ca3f5aba883e3cc93fa297422198520787b468a8
Gabe Radovsky <gabriel.radovsky@gmail.com>
2014-07-24 09:29:15 -0700

    add ButtonFormView to utils

---
+++ app/assets/javascripts/utils/button_form.js
@@ -0,0 +1,37 @@
+Backbone.ButtonFormView = Backbone.View.Extend({
+	formShowing: false,
+	
+	events: {
+		'click button': 'showForm',
+		'click .close': 'hideForm',
+		'submit': 'create',
+		'keydown textarea': 'createOnEnter'
+	},
+	
+	render: function() {
+		var content;
+		if (this.formShowing) {
+			content = this.formTemplate();
+		} else {
+			content = this.buttonTemplate();
+		}
+		this.$el.html(content);
+		this.delegateEvents();
+		return this;
+	},
+	
+	hideForm: function () {
+		this.formShowing = false;
+		this.render();
+	},
+	
+	showForm: function(event) {
+		event.preventDefault();
+		this.formShowing = true;
+		this.render();
+	},
+	
+	createOnEnter: function(event) {
+		if(event.keyCode === 13) this.create(event);
+	}
+});
