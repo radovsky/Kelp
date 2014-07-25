d647cec026adfea9575b83a5f14963114a1a1b18
Gabe Radovsky <gabriel.radovsky@gmail.com>
2014-07-24 23:37:12 -0700

    massaging database for heroku's pleasure

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,10 +5,10 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
-Business.create(name: 'Rotting Whale Carcass', location: 'Pacific Ocean', description: 'delicious!', category:'Steakhouse' price_range: 1, num_stars: 5)
+Business.create(name: 'Rotting Whale Carcass', location: 'Pacific Ocean', description: 'delicious!', category:'Steakhouse', price_range: '1', num_stars: '5')
 
-Business.create(name: 'Coral Reef', location: 'Arctic Ocean', description: 'everything is dead', category: 'Nightlife' price_range: 4, num_stars: 3)
+Business.create(name: 'Coral Reef', location: 'Arctic Ocean', description: 'everything is dead', category: 'Nightlife', price_range: '4', num_stars: '3')
 
-Business.create(name: 'Sunken Submarine', location: 'Marianas Trench', description: 'high-pressure environment', category: 'Breakfast & Brunch' price_range: 2, num_stars: 2)
+Business.create(name: 'Sunken Submarine', location: 'Marianas Trench', description: 'high-pressure environment', category: 'Breakfast & Brunch', price_range: '2', num_stars: '2')
 
-Business.create(name: 'Garbage Island', location: 'Sargasso Sea', description: 'pretty trashy', category: 'American' price_range: 1, num_stars: 4)
+Business.create(name: 'Garbage Island', location: 'Sargasso Sea', description: 'pretty trashy', category: 'American', price_range: '1', num_stars: '4')
